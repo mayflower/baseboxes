ef3ee9987ef5ea5caa686300f3c9c55af081c583
Michele Catalano <michele.catalano@mayflower.de>
2015-12-25 00:09:13 +0100

    change vmware tools from iso to apt package
    open issue: change scripts if we create other then ubuntu boxes.

---
+++ scripts/ubuntu/vmtools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case "$PACKER_BUILDER_TYPE" in
+
+virtualbox-iso|virtualbox-ovf)
+    mkdir /tmp/vbox
+    VER=$(cat /home/vagrant/.vbox_version)
+    mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox
+    sh /tmp/vbox/VBoxLinuxAdditions.run
+    umount /tmp/vbox
+    rmdir /tmp/vbox
+    rm /home/vagrant/*.iso
+    ;;
+
+vmware-iso|vmware-vmx)
+    apt-get install open-vm-tools
+    ;;
+
+*)
+    echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
+    echo "Known are virtualbox-iso|virtualbox-ovf|vmware-iso|vmware-ovf."
+    ;;
+
+esac
