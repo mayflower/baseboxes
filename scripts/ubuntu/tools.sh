796ddebfb88ead33a67d41115273776886def7c9
Michele Catalano <michele@catalano.de>
2016-02-01 13:58:48 +0100

    add package software-common-properties
    1. This software-common-properties provide add-apt-packages
    2. change the linux-headers to virtual package

--- scripts/ubuntu/tools.sh
+++ scripts/ubuntu/tools.sh
@@ -2,4 +2,6 @@
 
 # ensure the correct kernel headers are installed
 # and install some basic tools useful on nearly every dev vm
-apt-get -y install linux-headers-$(uname -r) git patch vim-nox htop bash-completion wget curl
+apt-get -y install linux-kernel-headers git patch vim-nox htop bash-completion wget curl gcc software-properties-common
+
+reboot
