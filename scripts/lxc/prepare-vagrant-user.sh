0bfebd2b5944924324a255f3d43fb9b95bd74d72
Michele Catalano <michele.catalano@mayflower.de>
2016-03-11 14:02:25 +0100

    fix lxc resolv.conf with dirty hack

--- scripts/lxc/prepare-vagrant-user.sh
+++ scripts/lxc/prepare-vagrant-user.sh
@@ -22,6 +22,8 @@ debug() {
   [ ! $DEBUG ] || echo "    [DEBUG] ${1}" >&2
 }
 
+# DIRTY HACK to prevent duplicate entry in /etc/resolv.conf
+sed -ie '5,$d' /etc/resolv.conf
 
 export VAGRANT_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 
